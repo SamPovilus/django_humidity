d5f54c3fa886983df193e58a560e26f742e7c7ac
Sam Povilus <kernel.development@povil.us>
2018-02-10 23:06:17 -0700

    setting up history display

--- humidity/humidity_display/urls.py
+++ humidity/humidity_display/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:hours>/',views.pasthours,name='pasthours'),
 ]
--- humidity/humidity_display/views.py
+++ humidity/humidity_display/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import datetime
+
+from django.utils import timezone
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +9,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the humidity display index.")
+
+def pasthours(request, hours):
+    now = str(timezone.now())
+    to = str(timezone.now() - datetime.timedelta(hours=hours))
+    return HttpResponse(" looking from %s to %s"% (now,to))
