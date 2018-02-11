dabfe057517f06274831b7e47a7326467580774d
Sam Povilus <kernel.development@povil.us>
2018-02-10 23:17:16 -0700

    simple humidity display works

--- humidity/humidity_display/views.py
+++ humidity/humidity_display/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 import datetime
 
 from django.utils import timezone
-
+from .models import Humidity
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the humidity display index.")
+    humidities = Humidity.objects.order_by('-log_date')[:5]
+    context = {'humidities': humidities}
+    return render(request, 'humidity_display/index.html', context)
 
 def pasthours(request, hours):
     now = str(timezone.now())
